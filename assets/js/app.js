bfa3d87e144582f2a2cda8ce2860a9aa578abd90
grahamwren <graham@maineisland.co>
2019-03-14 23:53:02 -0400

    Merge time_block updates in

--- assets/js/app.js
+++ assets/js/app.js
@@ -34,6 +34,30 @@ $(() => {
       .toFormat(DATETIME_FORMAT);
   }
 
+  function updateTimeBlocks(timeBlocks) {
+    const timeBlocksElement = $('.time-blocks');
+    if (timeBlocksElement[0]) {
+      timeBlocksElement.html(timeBlocks.map(getTimeBlockElement).join(''));
+    }
+  }
+
+  function getTimeBlockElement(timeBlock) {
+    return `<div data-tb-id="${timeBlock.id}" class="time-block-form">
+      <span class="time-block-inputs">
+        From: <input id="start_time" type="datetime-local" value="${getDateTimeLocal(timeBlock.start_time)}"/>
+        To: <input id="end_time" type="datetime-local" value="${(timeBlock.end_time && getDateTimeLocal(timeBlock.end_time)) || ''}"/>
+      </span>
+      <a
+        class="btn btn-info btn-xs update"
+        data-tb-id="${timeBlock.id}"
+      >Update</a>
+      <a
+        class="btn btn-danger btn-xs delete"
+        data-tb-id="${timeBlock.id}"
+      >Delete</a>
+    </div>`;
+  }
+
   $('.link').click(function () {
     window.location = $(this).data('href');
   });
@@ -41,27 +65,32 @@ $(() => {
   $('.manage').click(function () {
     const userId = $(this).data('user-id');
     $.ajax(`/api/v1/users/${userId}/manage`, {
-      method: 'post'
-    }).done(({data: user}) =>
-      $(`.user[data-user-id=${user.id}] .user-manager`)
-        .text(`Manager: ${user.manager.name || user.manager.email}`));
+      method: 'post',
+      success: ({data: user}) =>
+        $(`.user[data-user-id=${user.id}] .user-manager`)
+          .text(`Manager: ${user.manager.name || user.manager.email}`)
+    });
   });
 
   $('.toggle-working').click(function () {
     const taskId = $(this).data('task-id');
     if ($(this).data('start')) {
       $.ajax(`/api/v1/tasks/${taskId}/start_working`, {
-        method: 'post'
-      }).done(() => {
-        $(this).data('start', false);
-        $(this).text('Stop');
+        method: 'post',
+        success: () => {
+          $(this).data('start', false);
+          $(this).text('Stop');
+          updateTimeBlocks(data)
+        }
       });
     } else {
       $.ajax(`/api/v1/tasks/${taskId}/stop_working`, {
-        method: 'post'
-      }).done(() => {
-        $(this).text('Start');
-        $(this).data('start', true);
+        method: 'post',
+        success: ({data}) => {
+          $(this).text('Start');
+          $(this).data('start', true);
+          updateTimeBlocks(data)
+        }
       });
     }
   });
@@ -70,65 +99,48 @@ $(() => {
   const taskId = timeBlocksElement.data('task-id');
   if (timeBlocksElement[0]) {
     $.ajax(`/api/v1/tasks/${taskId}/time_blocks`, {
-      method: 'get'
-    }).done(({data}) => {
-      const timeBlocks = [];
-      for (const timeBlock of data) {
-        timeBlocks.push(
-          `<div data-tb-id="${timeBlock.id}" class="time-block-form">
-             <span class="time-block-inputs">
-               From: <input id="start_time" type="datetime-local" value="${getDateTimeLocal(timeBlock.start_time)}"/>
-               To: <input id="end_time" type="datetime-local" value="${(timeBlock.end_time && getDateTimeLocal(timeBlock.end_time)) || ''}"/>
-             </span>
-             <a
-              class="btn btn-info btn-xs update"
-              data-tb-id="${timeBlock.id}"
-             >Update</a>
-             <a
-               class="btn btn-danger btn-xs delete"
-               data-tb-id="${timeBlock.id}"
-             >Delete</a>
-          </div>`);
-      }
-      timeBlocksElement.html(timeBlocks.join(''));
+      method: 'get',
+      success: ({data: timeBlocks}) => {
+        updateTimeBlocks(timeBlocks);
 
-      $('.time-block-form .btn.update').click(function() {
-        const tbId = $(this).data('tb-id');
+        $('.time-block-form .btn.update').click(function() {
+          const tbId = $(this).data('tb-id');
 
-        const form = $(`.time-block-form[data-tb-id=${tbId}]`);
-        const start = DateTime.fromFormat(
-          form.find('input#start_time').val(),
-          DATETIME_FORMAT, {
-            zone: 'local'
-          }).toUTC().toMillis() / 1000;
-        const end = DateTime.fromFormat(
-          form.find('input#end_time').val(),
-          DATETIME_FORMAT, {
-            zone: 'local'
-          }).toUTC().toMillis() / 1000;
-        $.ajax(`/api/v1/tasks/${taskId}/time_blocks/${tbId}`, {
-          method: 'patch',
-          data: {
-            time_block: {
-              start_time: start,
-              end_time: end
+          const form = $(`.time-block-form[data-tb-id=${tbId}]`);
+          const start = DateTime.fromFormat(
+            form.find('input#start_time').val(),
+            DATETIME_FORMAT, {
+              zone: 'local'
+            }).toUTC().toMillis() / 1000;
+          const end = DateTime.fromFormat(
+            form.find('input#end_time').val(),
+            DATETIME_FORMAT, {
+              zone: 'local'
+            }).toUTC().toMillis() / 1000;
+          $.ajax(`/api/v1/tasks/${taskId}/time_blocks/${tbId}`, {
+            method: 'patch',
+            data: {
+              time_block: {
+                start_time: start,
+                end_time: end
+              }
             }
-          }
-        }).done(({data}) => {
-          const form = $(this).closest('.time-block-form');
-          form.find('input#start_time').val(getDateTimeLocal(data.start_time))
-          form.find('input#end_time').val(getDateTimeLocal(data.end_time))
+          }).done(({data}) => {
+            const form = $(this).closest('.time-block-form');
+            form.find('input#start_time').val(getDateTimeLocal(data.start_time))
+            form.find('input#end_time').val(getDateTimeLocal(data.end_time))
+          });
         });
-      });
 
-      $('.time-block-form .btn.delete').click(function() {
-        const tbId = $(this).data('tb-id');
-        $.ajax(`/api/v1/tasks/${taskId}/time_blocks/${tbId}`, {
-          method: 'delete'
-        }).done(() =>
-          $(this).closest('.time-block-form').remove()
-        );
-      });
+        $('.time-block-form .btn.delete').click(function() {
+          const tbId = $(this).data('tb-id');
+          $.ajax(`/api/v1/tasks/${taskId}/time_blocks/${tbId}`, {
+            method: 'delete'
+          }).done(() =>
+                    $(this).closest('.time-block-form').remove()
+          );
+        });
+      }
     });
   }
 });
